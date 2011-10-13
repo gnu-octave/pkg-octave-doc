be275984cef1a89a17c682958936c32a8b0d3047
carandraug <unknown>
2011-10-13 19:18:05 +0000

    gsl: adding DDDD_to_D template missing from revision 8729

--- inst/html_help_text.m
+++ inst/html_help_text.m
@@ -72,6 +72,7 @@ function html_help_text (name, outname, options = struct (), root = "")
       endif
 
       ## Run makeinfo
+pwd
       [text, status] = __makeinfo__ (text, "html", seealso);
       if (status != 0)
         error ("html_help_text: couln't parse file '%s'", name);
