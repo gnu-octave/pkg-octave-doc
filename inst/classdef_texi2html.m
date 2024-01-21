6be88e3d71d33d9f272dff01d07afd1982d1690f
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-01-21 03:42:47 +0200

    classdef_texi2html: new function to handle classdef objects including class methods

---
+++ inst/classdef_texi2html.m
@@ -0,0 +1,176 @@
+## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
+##
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {pkg-octave-doc} {} classdef_texi2html (@var{clsname}, @var{pkgfcns}, @var{info})
+##
+## Generate HTML page for a class definition.
+##
+## @code{function_texi2html} requires three input arguments: @var{clsname}, a
+## char string with the class' name; @var{pkgfcns}, a cell array with all
+## available functions of a package; and @var{info}, a structure with relevant
+## information about the package, which the function @var{clsname} belongs to.
+##
+## @var{pkgfcns} can be either a @math{Nx2} or a @math{Nx3} cell array, whose
+## 1st column list all available function names, the 2nd column list the each
+## function's category, and the 3rd column contains the URL to the function's
+## source code.  @var{pkgfcns} is used to relative references to other pages of
+## functions which are listed in the @qcode{See also} tag.  When a 3rd column is
+## present, @code{function_texi2html} uses it to add a source code link of the
+## the function in @var{clsname}.
+##
+## The @var{info} structure requires at least the following fields:
+##
+## @multitable @columnfractions 0.2 0.8
+## @headitem Field Name @tab Description
+## @item @qcode{PKG_ICON} @tab The relative reference to the package's logo
+## image which must be either in .svg or .png format and it is located in the
+## newly created @qcode{assets/} folder inside the working directory.
+##
+## @item @qcode{PKG_NAME} @tab The package's name (e.g. "pkg-octave-doc")
+##
+## @item @qcode{PKG_TITLE} @tab The package's title (e.g. "Octave Package
+## Documentation")
+##
+## @item @qcode{OCTAVE_LOGO} @tab The relative reference to Octave's logo, also
+## located inside the @qcode{assets/} folder.
+##
+## @end multitable
+##
+## To generate a suitable @math{Nx2} cell array for a specific package, use the
+## @code{package_texi2html} function and to populate is with the 3rd column use
+## @code{find_GHurls}.  The @var{info} structure can also be created with
+## @code{package_texi2html}.
+##
+## @code{function_texi2html} depends on the @qcode{texi2html} command line tool
+## version 1.82, which must be installed and available on the system's $PATH,
+## and the generated HTML code is based on the @qcode{function_template.html}
+## and @qcode{default.html} layouts.
+##
+## @seealso{package_texi2html, function_texi2html, find_GHurls, build_DEMOS}
+## @end deftypefn
+
+function classdef_texi2html (clsname, pkgfcns, info)
+
+  if (nargin != 3)
+    print_usage ();
+  endif
+
+  if (! ischar (clsname))
+    print_usage ();
+  endif
+
+  if (! iscell (pkgfcns))
+    print_usage ();
+  endif
+
+  if (! isstruct (info))
+    print_usage ();
+  endif
+
+  if (! isfield (info, "PKG_ICON") || ! isfield (info, "PKG_NAME") || ...
+      ! isfield (info, "PKG_TITLE") || ! isfield (info, "OCTAVE_LOGO"))
+    print_usage ();
+  endif
+
+  ## Check is clsname is an actual classdef
+  try
+    MTDS = methods (clsname);
+  catch
+    error ("classdef_texi2html: %s is not classdef name", clsname);
+  end_try_catch
+
+  ## Add try catch to help identify classdef file that caused an issue
+  ## during batch processing all functions in a package with package_texi2html
+  try
+    ## Build the HTML code for class constructor
+    cls_text = __texi2html__ (MTDS{1}, pkgfcns);
+
+    ## Find the category the classdef belongs to
+    fcn_idx = find (strcmp (pkgfcns(:,1), clsname));
+    catname = pkgfcns{fcn_idx, 2};
+
+    ## Add link to classdef's source code (if applicable)
+    if (size (pkgfcns, 2) == 3)
+      url = pkgfcns{fcn_idx, 3};
+      url_text = strcat (["<p><strong>Source Code: </strong>\n"], ...
+                         ["  <a href=""", url, """>", clsname, "</a>\n</div>"]);
+      cls_text = strrep (cls_text, "</div>", url_text);
+    endif
+  catch
+    printf ("Unable to process class constructor %s:\n %s\n", MTDS{1}, lasterr);
+    return;
+  end_try_catch
+
+  ## Build HTML code for remaining methods
+  for m = 2:numel (MTDS)
+    method_name = [clsname "." MTDS{m}];
+    try
+      mtds_text = __texi2html__ (method_name, pkgfcns);
+      ## Load methods template
+      mtds_template = fileread (fullfile ("_layouts", "method_template.html"));
+      mtds_template = strrep (mtds_template, "{{METHOD_NAME}}", MTDS{m});
+      mtds_template = strrep (mtds_template, "{{METHOD_HELP}}", mtds_text);
+    catch
+      mtds_template = "";
+      printf ("Unable to process method %s of class %s:\n %s\n", ...
+              MTDS{m}, clsname, lasterr);
+    end_try_catch
+    cls_text = [cls_text "\n" mtds_template];
+  endfor
+
+  ## Add DEMOS (if applicable)
+  demo_txt = build_DEMOS (clsname);
+  cls_text = [cls_text "\n" demo_txt];
+
+  ## Populate function template with package info
+  fnc_template = fileread (fullfile ("_layouts", "classdef_template.html"));
+  fnc_template = strrep (fnc_template, "{{PKG_ICON}}", info.PKG_ICON);
+  fnc_template = strrep (fnc_template, "{{PKG_NAME}}", info.PKG_NAME);
+  fnc_template = strrep (fnc_template, "{{PKG_TITLE}}", info.PKG_TITLE);
+  fnc_template = strrep (fnc_template, "{{CAT_NAME}}", catname);
+  fnc_template = strrep (fnc_template, "{{OCTAVE_LOGO}}", info.OCTAVE_LOGO);
+  fnc_template = strrep (fnc_template, "{{CLS_NAME}}", clsname);
+  fnc_template = strrep (fnc_template, "{{CLS_TEXT}}", cls_text);
+
+  ## Populate default template
+  default_template = fileread (fullfile ("_layouts", "default.html"));
+  output_str = default_template;
+  TITLE = sprintf ("%s: %s", info.PKG_TITLE, clsname);
+  output_str = strrep (output_str, "{{TITLE}}", TITLE);
+  output_str = strrep (output_str, "{{BODY}}", fnc_template);
+
+  ## Write html to file
+  try
+    fid = fopen ([clsname ".html"], "w");
+    fprintf (fid, "%s", output_str);
+    fclose (fid);
+  catch
+    printf ("Unable to process class %s:\n %s\n", clsname, lasterr);
+  end_try_catch
+
+endfunction
+
+%!error classdef_texi2html (1)
+%!error classdef_texi2html (1, 2)
+%!error classdef_texi2html (1, cell (2))
+%!error classdef_texi2html (1, cell (2), struct("PKG_ICON", {""}, ...
+%! "PKG_NAME", {""}, "PKG_TITLE", {""}, "OCTAVE_LOGO", {""}))
+%!error classdef_texi2html ("find_GHurls", "text" , struct("PKG_ICON", {""}, ...
+%! "PKG_NAME", {""}, "PKG_TITLE", {""}, "OCTAVE_LOGO", {""}))
+%!error classdef_texi2html ("find_GHurls", cell (2) , struct("field", {""}, ...
+%! "PKG_NAME", {""}, "PKG_TITLE", {""}, "OCTAVE_LOGO", {""}))
